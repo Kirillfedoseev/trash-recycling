e6019fc869d363443cf23494634d94fc0e862bd9
Nadezhda <enotpetya@gmail.com>
2020-05-04 17:37:25 +0300

    Activity ItemDescription added

---
+++ app/src/main/java/com/innopolis/trashrecycle/ui/ItemDescriptionActivity.kt
@@ -0,0 +1,13 @@
+package com.innopolis.trashrecycle.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.innopolis.trashrecycle.R
+
+class ItemDescriptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_description)
+    }
+}
