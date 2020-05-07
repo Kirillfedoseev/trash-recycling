f1899ffedd5f3314b2ce943ebd25071557728dff
oninbo <oninbo@gmail.com>
2020-05-07 20:48:57 +0300

    added map activity

---
+++ app/src/main/java/com/innopolis/trashrecycle/ui/MapActivity.kt
@@ -0,0 +1,11 @@
+package com.innopolis.trashrecycle.ui
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
