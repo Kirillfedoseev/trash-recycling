3ecc233dfcc2977e02492018e4e4bd1ac571cb6c
anastasiakirdyasheva <kirdyasheva98@gmail.com>
2020-05-13 08:47:33 +0300

    Revert "Types of garbage with its relations to codes of the packages."
    This reverts commit ecf9a12bff123ad0ca9a16ca70cbe4263b9d483d.

---
+++ app/src/main/java/com/innopolis/trashrecycle/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.app.studentsproject.di
+
+
+import org.koin.dsl.module
+
+fun createAppModule() = module {
+}
---
+++ app/src/main/java/com/innopolis/trashrecycle/ui/ItemDescriptionActivity.kt
@@ -0,0 +1,17 @@
+package com.innopolis.trashrecycle.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.innopolis.trashrecycle.R
+
+class ItemDescriptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_description)
+        initInfo()
+        initListeners()
+    }
+    private fun initListeners() {}
+    private fun initInfo() {}
+}
